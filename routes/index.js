6901bd09b10619eb919ef9f0d0309d62b1d152b4
Ewomazino Odah <zinoodah@gmail.com>
2024-05-17 00:33:29 +0100

    style: clean up index and categories page

--- routes/index.js
+++ routes/index.js
@@ -24,21 +24,10 @@ router.get('/', function(req, res, next) {
 
     return -1;
   });
-
-  // let categoryPosts = Array.from(new Set(data.map((post) => post.slug).sort()));
-  // let categoryPosts = data.map(function(post){
-  //   let [slug] = post.slug;
-  //   let [cat] = post.category
-  // })
-  // console.log(categoryPosts)
-
   
   let navigationLinks = Array.from(new Set(data.map((post) => post.category).sort()));
   
   let categoryFilteredPosts = {};
-  // navigationLinks.forEach(category =>{
-  //   categoryFilteredPosts[category] = data.filter(post => post.category === category);
-  // })
 
   data.forEach(post => {
     const category = post.category;
@@ -47,16 +36,12 @@ router.get('/', function(req, res, next) {
     }
     categoryFilteredPosts[category].push(post);
   })
-  console.log(categoryFilteredPosts)
-
-  // let category = data.map((post) => post.category).sort());
+  // console.log(categoryFilteredPosts)
 
   let dates = data.map(function(post) {
     let [year, month] = post.created_at.split('-');
     return new Date(`${year}-${month}-01`);
   });
-
-  // let category = data.filter((post) => post.category)
 
   res.render('index', {
     title: 'She Code Queens',
@@ -65,8 +50,6 @@ router.get('/', function(req, res, next) {
     featuredPosts: data.filter((post) => post.is_featured),
     archives: Array.from(new Set(dates)),
     categories: categoryFilteredPosts
-    // categories: data.filter((post) => post.slug),
-    // categories: categoryPosts,
   });
 
 });
